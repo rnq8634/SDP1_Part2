8dd566adb97c5bc5e5cfaee4c6112cf6d3216634
ygv0305 <ygv0305@autuni.ac.nz>
2024-06-07 23:05:28 +1200

    Update PlayerRecover.java

--- SDP1_PART2/src/net/sdp1/main/PlayerRecover.java
+++ SDP1_PART2/src/net/sdp1/main/PlayerRecover.java
@@ -29,33 +29,29 @@ public class PlayerRecover
     //taking a rest
     public void recoverHealth() 
     {
-        if(ui.player.recoverRemaining >= 1 ) 
+        if(ui.player.recoverRemaining >= 1)
         {
-            System.out.println("Do you want to recover?");
-            System.out.println("[RECOVER] " + ui.player.recoverRemaining);
-            
-            System.out.println("[1] Yes, my body's beat...\n[2] No, I'm brimming with QI!!!");
-      
-                //player takes a rest
-                if(ui.player.health < ui.player.maxHealth) 
-                {
-                    int hpRestored = (int) (Math.random() * (ui.player.qi/4 + 2) + 10);
-                    ui.player.health += hpRestored;
-                    if(ui.player.health > ui.player.maxHealth)
-                        ui.player.health = ui.player.maxHealth;
-                    
-                    System.out.println("You did Restorative Breathing and regained "+ hpRestored + " health.");
-                    
-                    System.out.println("You're now at " + ui.player.health + "/" + ui.player.maxHealth + " health.");
-                    
-                    ui.player.recoverRemaining--;
-                }else if(ui.player.health == ui.player.maxHealth) 
-                {
-                    System.out.println("There is no reason to rest since you are at full health!");
-                }
-            }else 
+            //player takes a rest
+            if(ui.player.health < ui.player.maxHealth) 
             {
-                System.out.println("You felt that there is no reason to rest.");
+                int hpRestored = (int) (Math.random() * (ui.player.qi/4 + 2) + 10);
+                ui.player.health += hpRestored;
+                if(ui.player.health > ui.player.maxHealth)
+                    ui.player.health = ui.player.maxHealth;
+
+                ui.gameTextArea.setText("You're now at " + ui.player.health + "/" + ui.player.maxHealth + " health.");
+
+                //choices
+                ui.buttonEditor(ui.action1, "<", e -> ui.gameGUI());
+
+                ui.buttonEditor(ui.action2, "", null);
+
+                ui.buttonEditor(ui.action3, "", null);
+
+                ui.buttonEditor(ui.action4, "", null); 
+
+                ui.player.recoverRemaining--;
             }
         }
     }
+}
